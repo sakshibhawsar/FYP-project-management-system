d8ce4871319e50854e8c8914c0539d239743d296
uditmaltare <uditmaltare@gmail.com>
2026-04-11 20:25:17 +0530

    testing webhook

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ connectDB();
 //-------------------
 //START SERVER
 //-------------------
-const PORT = process.env.PORT||4000;
+const PORT = process.env.PORT||5000;
 
 const server=app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
